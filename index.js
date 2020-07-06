febf609fd8b125fa87578220c4f52840c95ac58e
Kirill Krupina <kirill.krupina26@gmail.com>
2020-07-06 21:48:04 +0300

    Simple example with requests

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 
 const app = express();
+const products = ['Apple', 'Melon', 'Banana'];
 
-app.get('/', (req, res, next) =>{
+app.get('/', (req, res, next) => {
     res.send(`It's working!`);
 });
 
-app.listen(5000, _ =>{
-    console.log(`Server starting... ${new Date()}`);
-    
+app.get('/products', (req, res, next) => {
+    console.log('Page', req.query.page);       //localhost:5000/products?page=#
+
+    res.json({products});
+});
+
+app.get('/products/:id', (req, res, next) => {
+    if (products[req.params.id]) {
+        res.send(products[req.params.id]);
+    } else {
+        res.status(404).send('Product not found');
+    }
+});
+
+app.listen(5000, _ => {
+    console.log(`Server is starting... ${new Date()}`);
 });
